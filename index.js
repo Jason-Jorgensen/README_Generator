733dd185120cb3a4f8c7b0630b15693da9235dc9
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-02-14 22:45:39 -0700

    finished index.js file with prompt questions and link to the generateMarkdown.js

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // array of questions for user
-const questions = [
+inquirer
+    .prompt([
+        {
+            type: 'input' ,
+            name:'userName',
+            message:'What is your GitHub username?',
+            default: 'Jason-Jorgensen',
+        },
+        {
+            type: 'input' ,
+            name:'emailAddress',
+            message:'What is your Email Address?',
+            default: 'jasoncjorgensen@gmail.com',
+        },
+        {
+            type: 'input' ,
+            name:'projectName',
+            message:"What is your project's name?",
+        },
+        {
+            type: 'editor' ,
+            name:'projectDescription',
+            message:'Please write a short description of your project',
+        },
+        {
+            type: 'list' ,
+            name:'licenseType',
+            message:'What kind of license should your project have?',
+            choices: ["MIT", "Apache", "Eclipse", "Mozilla", "Unlicense"],
+        },
+        {
+            type: 'input' ,
+            name:'runInstall',
+            message:'What command should be run to install dependencies?',
+            default: 'npm i',
+        },
+        {
+            type: 'input' ,
+            name:'runTest',
+            message:'What command should be run to run tests?',
+            default: 'npm test',
+        },
+        {
+            type: 'input' ,
+            name:'contributing',
+            message:'What does the user need to know about contributing to the repo?',
+        },
+    ]).then((data) =>{
+        console.log(data);
+        const makeReadMe = generateMarkdown(data);
+        fs.writeFileSync("README.md",makeReadMe)
+    })
 
-];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
